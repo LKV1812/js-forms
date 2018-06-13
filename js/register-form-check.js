98e47ac013152f6e99401a1d1b85eaaddeda9f52
Konstantin Ledik <Konstantin.Ledik@mail.ru>
2018-06-13 16:43:00 +0500

    remarks corrected

--- js/register-form-check.js
+++ js/register-form-check.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		// Переменные модуля
 		var _registerForm = $('#register-form'),
 			_patternEmail = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
-			_inputLogin = $('[name="name"]'),//поле ввода логина
+			_inputLogin = $('[name="email"]'),//поле ввода логина
 			_inputPassword = $('[name="password"]');// поле ввода пароля
 			
 		// Метод инициализации (запуска) модуля
@@ -23,65 +23,80 @@ $(document).ready(function() {
 			
 			var _inputLoginValue = _inputLogin.val().trim(),//value поля ввода логина
 				_inputPasswordValue = _inputPassword.val(),//value поля ввода пароля
+				_tooltip = $('<div class="error error-hide"></div>'),
 				_errorText = "",
-				_tooltipe = $('<div class="error error-hide"></div>'),
-				_emailBusy = $('<div class="error error--with-desc error-hide">Данный email уже занят</div><div class="error-description"><p>Используйте другой email чтобы создать новый аккаунт.</p><p>Или воспользуйтесь <a href="#">восстановлением пароля</a>, чтобы войти на сайт.</p></div>');
+				_emailBusy = $('<div class="error error--with-desc error-hide">Данный email уже занят</div><div class="error-description"><p>Используйте другой email чтобы создать новый аккаунт.</p><p>Или воспользуйтесь <a href="#">восстановлением пароля</a>, чтобы войти на сайт.</p></div>');	
+
 			if (_inputLoginValue.length === 0) {
 				
-				$(".error").remove();
 				_errorText = _inputLogin.data('email-error');
-				_tooltipe.text(_errorText);
-				$('.plate__heading').after(_tooltipe);
-				_tooltipe.fadeIn();
+				var errorEmailEmpty = _tooltip.clone().text(_errorText);
+				$(".plate__heading").after(errorEmailEmpty);
+				errorEmailEmpty.fadeIn();
 				event.preventDefault();
+
+				// по сабмиту удаляем ошибку, чтобы она не множилась
+				_registerForm.on("submit", function(){
+					errorEmailEmpty.remove();
+				});
+
+				_inputLogin.on("focus", function(){
+					errorEmailEmpty.fadeOut();
+				});
 
 			} else if (!(_patternEmail.test(_inputLoginValue) && _inputLoginValue.length > 0)) {
-				
-				$(".error").remove();
-				_errorText = "Неверный формат email";
-				_tooltipe.text(_errorText);
-				_inputLogin.before(_tooltipe);
+
+				_errorText = _inputLogin.data("email-error-format");
+				var errorEmailFormat = _tooltip.clone().text(_errorText);
+				$(".plate__heading").after(errorEmailFormat);
+				errorEmailFormat.fadeIn();
 				event.preventDefault();
 
-			}
+				// по сабмиту удаляем ошибку, чтобы она не множилась
+				_registerForm.on("submit", function(){
+					errorEmailFormat.remove();
+				});
+
+				_inputLogin.on("focus", function(){
+					errorEmailFormat.fadeOut();
+				});
+
+			} 
 
 			if (_inputPasswordValue.length === 0) {
 				
-				$(".error").remove();
 				_errorText = _inputPassword.data('password-error');
-				_tooltipe.text(_errorText);
-				_inputLogin.before(_tooltipe);
-				_tooltipe.fadeIn();
+				var errorPasswordEmpty = _tooltip.clone().text(_errorText);
+				_inputLogin.before(errorPasswordEmpty);
+				errorPasswordEmpty.fadeIn();
 				event.preventDefault();
+
+				// по сабмиту удаляем ошибку, чтобы она не множилась
+				_registerForm.on("submit", function(){
+					errorPasswordEmpty.remove();
+				});
+
+				_inputPassword.on("focus", function(){
+					errorPasswordEmpty.fadeOut();
+				});
 
 			}
 
 			if (_inputLoginValue == "mail@mail.com" && _inputPasswordValue.length > 0) {
 				
-				$(".error").remove();
-				$(".error-description").remove();
 				_inputLogin.before(_emailBusy);
 				_emailBusy.fadeIn();
 				event.preventDefault();
 
+				_registerForm.on("submit", function(){
+					_emailBusy.remove();
+				});
+
 			} else {
 				
-				$(".error--with-desc").remove();
-				$(".error-description").remove();
+				_emailBusy.fadeOut();
 
 			}
-
-			_inputLogin.on("focus", function(){
-				
-				_tooltipe.remove();
-
-			});
-
-			_inputPassword.on("focus", function(){
-
-				_tooltipe.remove();
-				
-			});
 		
 		}
 
